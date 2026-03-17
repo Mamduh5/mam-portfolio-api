e351cb795e52a5d8581a75ee8956fbe5e1305485
Mamduh <mamduh2542@gmail.com>
2026-03-17 18:46:15 +0700

    update

--- src/interfaces/routes/visitRoutes.js
+++ src/interfaces/routes/visitRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const rateLimit = require("express-rate-limit")
 
 const visitController = require("../controllers/visitController")
 
-router.post("/", visitController.createVisit)
+const visitLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 20, // max 20 requests per IP per minute
+})
+
+
+router.post("/", visitLimiter, visitController.createVisit)
 
 module.exports = router
 
